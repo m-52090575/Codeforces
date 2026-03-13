640c6a6270460af094f818257658fffcea0ef7e0
Mary <218518147+m-52090575@users.noreply.github.com>
2026-03-12 22:02:52 -0700

    use proper grammer for constant name in WayTooLongWords.java

--- WayTooLongWords.java
+++ WayTooLongWords.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class WayTooLongWords {
 
-	public static final int WORD_MAX_LENGTH = 10;
+	public static final int MAX_WORD_LENGTH = 10;
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = Integer.parseInt(scan.nextLine());
 		for (int i = 0; i < n; i++) {
 			String word = scan.nextLine();
-			if (word.length() > WORD_MAX_LENGTH) {
+			if (word.length() > MAX_WORD_LENGTH) {
 				word = word.substring(0, 1)
 						+ (word.length() - 2)
 						+ word.substring(word.length() - 1);
